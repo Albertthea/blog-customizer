382943c0ce1b3376ad27627dc6afca755fdddf95
Iana Spiridonova <iana.spiridonova01@gmail.com>
2025-03-12 11:00:46 +0100

    code cleaned

--- src/ui/arrow-button/ArrowButton.tsx
+++ src/ui/arrow-button/ArrowButton.tsx
@@ -1,5 +1,4 @@
 import arrow from 'src/images/arrow.svg';
-import { forwardRef } from 'react';
 import styles from './ArrowButton.module.scss';
 import clsx from 'clsx';
 
@@ -10,28 +9,26 @@ type ArrowButtonProps = {
 	onClick: OnClick;
 };
 
-export const ArrowButton = forwardRef<HTMLDivElement, ArrowButtonProps>(
-	({ isOpen, onClick }) => {
-	  return (
-		<div
-		  role="button"
-		  aria-label="Открыть/Закрыть форму параметров статьи"
-		  tabIndex={0}
-		  className={clsx(styles.container, {
-			[styles.container_open]: isOpen,
-		  })}
-		  onClick={(e) => {
-			onClick(e);
-		  }}
-		>
-		  <img
-			src={arrow}
-			alt="иконка стрелки"
-			className={clsx(styles.arrow, {
-			  [styles.arrow_open]: isOpen,
-			})}
-		  />
-		</div>
-	  );
-	}
-);
+export const ArrowButton = ({ isOpen, onClick }: ArrowButtonProps) => {
+	return (
+	<div
+		role="button"
+		aria-label="Открыть/Закрыть форму параметров статьи"
+		tabIndex={0}
+		className={clsx(styles.container, {
+		[styles.container_open]: isOpen,
+		})}
+		onClick={(e) => {
+		onClick(e);
+		}}
+	>
+		<img
+		src={arrow}
+		alt="иконка стрелки"
+		className={clsx(styles.arrow, {
+			[styles.arrow_open]: isOpen,
+		})}
+		/>
+	</div>
+	);
+};
